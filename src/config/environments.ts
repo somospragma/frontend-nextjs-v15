c82bdbe64b09f1ccd06faef82204eb096b1ef27e
Santiago Betancur Duque <santib.2001@hotmail.com>
2025-01-02 20:37:35 -0500

    chore: add updated environment variables configuration

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -1,13 +1,2 @@
-export const BASE_URL = process.env.BASE_URL as string;
-export const NEXT_PUBLIC_AUTH_CLIENT_ID = process.env.NEXT_PUBLIC_AUTH_CLIENT_ID as string;
-export const NEXT_PUBLIC_AUTH_TENANT_ID = process.env.NEXT_PUBLIC_AUTH_TENANT_ID as string;
-export const NEXT_PUBLIC_AUTH_REDIRECT_URI = process.env.NEXT_PUBLIC_AUTH_REDIRECT_URI as string;
-export const NEXT_PUBLIC_MERCURIO_APP_API_KEY = process.env
-  .NEXT_PUBLIC_MERCURIO_APP_API_KEY as string;
-export const NEXT_PUBLIC_MERCURIO_AUTH_ENDPOINT = process.env
-  .NEXT_PUBLIC_MERCURIO_AUTH_ENDPOINT as string;
-export const NEXT_PUBLIC_MERCURIO_CLUSTER = process.env.NEXT_PUBLIC_MERCURIO_CLUSTER as string;
-export const NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN = process.env
-  .NEXT_CONTENTFUL_ACCESS_TOKEN as string;
-export const NEXT_PUBLIC_CONTENTFUL_SPACE_ID = process.env
-  .NEXT_PUBLIC_CONTENTFUL_SPACE_ID as string;
+export const PREFIX_PATH = process.env.NEXT_PUBLIC_PREFIX_PATH as string;
+export const PRIVATE_ENDPOINT = process.env.NEXT_PRIVATE_ENDPOINT as string;
