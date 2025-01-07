669c3d0f65d48fe962328ae7b0e734905ea868db
Santiago Betancur Duque <santib.2001@hotmail.com>
2025-01-07 09:57:37 -0500

    chore: improved next export config to most friendly deploy build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  trailingSlash: true,
+  assetPrefix: '',
+  basePath: '',
   images: { unoptimized: true }
 };
 
